fb950d38ac96bda68fb1ff39c458632b92a28eae
molinamiguel <md253@protonmail.com>
2016-06-01 22:55:56 -0400

    Modifications to deploy mmco bot

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('Hello there! My name is Juan, Miguel\'s personal bot. To get started, enter \"Yo bot\"')
                 .then(() => 'speak');
         }
     },
